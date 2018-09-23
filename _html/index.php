a03a0a854ebddcb9a8fe7814dd83bac7930e0e39
Kleber Santos <j.kleber.s@gmail.com>
2018-09-22 22:38:28 -0300

    site

--- _html/index.php
+++ _html/index.php
@@ -423,6 +423,191 @@
 
     </section>
 
+    <section class="bloco4">
+
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+
+                    <span>_Faça do seu jeito</span>
+
+                    <h2>Conheça os Cursos</h2>
+
+                    <p>Na Casa Garcia Gourmet Centro Culinário você conta com vários cursos nas áreas da Confeitaria, Panificação e Sorveteria.</p>
+
+                    <form action="">
+                        <div class="box">
+                            <select name="" id="">
+                                <option value="">Selecione uma opção</option>
+                                <option value="">Selecione uma opção</option>
+                                <option value="">Selecione uma opção</option>
+                                <option value="">Selecione uma opção</option>
+                                <option value="">Selecione uma opção</option>
+                            </select>
+                            <input type="submit" class="bt--padrao" value="CONHECER" />
+                        </div>
+                    </form>
+                
+                </div>
+            </div>
+        </div>
+
+    </section>
+
+    <section class="bloco3 ingresso_sec">
+
+        <div class="container">
+
+            <div class="row">
+                <div class="col-12">
+                    <span class="pre">_Venha para o Centro Culinário Casa Garcia Gourmet</span>
+                    <h2>Formas de <span>Ingresso</span></h2>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12">
+                    <div class="owl-carousel owl-carousel-vantagens owl-theme">
+                        <div class="item">
+                            <img src="build/images/icon5.png" alt="">
+                            <h3>Deixe seu interesse</h3>
+                            <p>Faça sua pré-reserva em um de nossos cursos através do formulário de reservas de cursos.</p>
+                            <a href="#!">ENTRE EM CONTATO</a>
+                        </div>
+
+                        <div class="item">
+                            <img src="build/images/icon6.png" alt="">
+                            <h3>Pagamento no Local </h3>
+                            <p>Faça o pagamento do seu curso escolhido em nossa sede.</p>
+                            <a href="#!">SAIBA COMO CHEGAR</a>
+                        </div>
+
+                        <div class="item">
+                            <img src="build/images/icon7.png" alt="">
+                            <h3>Entre em Contato</h3>
+                            <p>Quer fazer um de nossos cursos? Ligue e reserve sua vaga agora mesmo.</p>
+                            <a href="https://api.whatsapp.com/send?phone=5585999100644"><i class="fa fa-whatsapp"></i> (85) 99910.0644</a>
+                        </div>
+
+                        <div class="item">
+                            <img src="build/images/icon1.png" alt="">
+                            <h3>Deixe seu interesse</h3>
+                            <p>Faça sua pré-reserva em um de nossos cursos através do formulário de reservas de cursos.</p>
+                            <a href="#!">ENTRE EM CONTATO</a>
+                        </div>
+
+                        <div class="item">
+                            <img src="build/images/icon2.png" alt="">
+                            <h3>Pagamento no Local </h3>
+                            <p>Faça o pagamento do seu curso escolhido em nossa sede.</p>
+                            <a href="#!">ENTRE EM CONTATO</a>
+                        </div>
+
+                        <div class="item">
+                            <img src="build/images/icon3.png" alt="">
+                            <h3>Entre em Contato</h3>
+                            <p>Quer fazer um de nossos cursos? Ligue e reserve sua vaga agora mesmo.</p>
+                            <a href="#!">ENTRE EM CONTATO</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+    </section>
+
+    <section class="bloco2 conheca_sec">
+
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 col-12">
+                    <div class="box">
+                        <span>_Tudo que você precisa</span>
+                        <h2>
+                            <a href="#">
+                            Conheça a<br />
+                            Casa Garcia Gourmet
+                            </a>
+                        </h2>
+                        <p>Do mesmo modo, a contínua expansão de nossa atividade facilita a criação
+                         do levantamento das variáveis envolvidas. As experiências acumuladas demonstram um consenso.
+                        </p>
+                        <a href="#!" class="bt--padrao">CONHEÇA MAIS</a>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-12 center">
+                    <img src="build/images/ehnozes.png" alt="img">
+                </div>
+            </div>
+        </div>
+
+    </section>
+
+    <section class="galleria d-flex justify-content-between flex-wrap">
+
+        <div class="item"><img src="build/images/a2.jpg" alt="img"></div>
+        <div class="item"><img src="build/images/a3.jpg" alt="img"></div>
+        <div class="item"><img src="build/images/a4.jpg" alt="img"></div>
+        <div class="item"><img src="build/images/a5.jpg" alt="img"></div>
+        <div class="item"><img src="build/images/a1.jpg" alt="img"></div>
+        <div class="item"><img src="build/images/a6.jpg" alt="img"></div>
+        <div class="item"><img src="build/images/a8.jpg" alt="img"></div>
+        <div class="item"><img src="build/images/a7.jpg" alt="img"></div>
+    
+    </section>
+
+    <section class="bloco8">
+
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <span class="pre">_Casa Garcia Gourmet</span>
+                    <h2><span>Conheça as Unidades</span></h2>
+                </div>
+            </div>
+            <div class="row">
+
+                <div class="col-12 d-flex justify-content-around">
+
+                    <div class="item">
+
+                        <img src="build/images/cg.jpg" alt="img">
+                        <div class="content">
+                            <span>Unidade Centro Culinário</span>
+                            <span class="local">Montese</span>
+
+                            <address> 
+                                Av. dos Expedicionários, 5220
+                                Montese, Fortaleza - CE, 60410-410
+                            </address>
+                            <a href="#!">Veja no mapa</a>
+                        </div>
+
+                    </div>
+
+                    <div class="item">
+
+                        <img src="build/images/cg.jpg" alt="img">
+                        <div class="content">
+                            <span>Unidade</span>
+                            <span class="local">Centro</span>
+
+                            <address> 
+                            R. Tereza Cristina, 1213
+Centro, Fortaleza - CE, 60015-141
+                            </address>
+                            <a href="#!">Veja no mapa</a>
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+
+    </section>
+
 </div>
 
 
